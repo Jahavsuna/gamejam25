af9dfac5f426c4950ed678f1af7327dcedad9628
Yahav Lavie <44113279+Jahavsuna@users.noreply.github.com>
2025-07-31 13:45:23 +0300

    Added lines, and they are now auto-plotted

---
+++ line.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+const MIN_X = -100
+const MAX_X = 740 # 640 + 100
+
+@export var outer_width: int = 110
+@export var edge_width: int = 20
+@export var road_width: int = 380
+
+@export var outer_color: Color
+@export var edge_color: Color
+@export var road_color: Color
+
+@export var x_offset: float = 0.0
+@export var y_offset: float = 0.0
+@export var line_width: int = 8
+
+func _ready() -> void:
+	pass
+
+func _draw() -> void:
+	var ttl_len = 2 * outer_width + 2 * edge_width + road_width
+	if ttl_len != 640:
+		print("Warning: bade line width: " + str(ttl_len))
+	if outer_color == null or edge_color == null or road_color == null:
+		print("ERROR: Line has no defined colors.")
+	var outer_start = Vector2(MIN_X, y_offset)
+	var left_outer_end = Vector2(x_offset + outer_width, y_offset)
+	var left_edge_end = Vector2(left_outer_end.x + edge_width, y_offset)
+	var road_end = Vector2(left_edge_end.x + road_width, y_offset)
+	var right_edge_end = Vector2(road_end.x + edge_width, y_offset)
+	var right_outer_end = Vector2(MAX_X, y_offset)
+	draw_line(outer_start, left_outer_end, outer_color, line_width)
+	draw_line(left_outer_end, left_edge_end, edge_color, line_width)
+	draw_line(left_edge_end, road_end, road_color, line_width)
+	draw_line(road_end, right_edge_end, edge_color, line_width)
+	draw_line(right_edge_end, right_outer_end, outer_color, line_width)
--- scripts/track_section_manager.gd
+++ scripts/track_section_manager.gd
@@ -1,10 +1,14 @@
 extends Node2D
 
-var track_data: Array = []
 const TRACK_DATA_FILE: String = "res://assets/tracks/first_level.json"
+var track_data: Array = []
+var track_lines: Array = []
+
+const LineScene: PackedScene = preload("res://scenes/Line.tscn")
 
 func _ready() -> void:
 	_load_track_data()
+	_generate_track_from_data()
 
 func _load_track_data() -> void:
 	var file = FileAccess.open(TRACK_DATA_FILE, FileAccess.READ)
@@ -16,3 +20,30 @@ func _load_track_data() -> void:
 	else:
 		print("ERROR: Failed to load JSON from " + TRACK_DATA_FILE)
 	file.close()
+
+func _configure_line_colors(line: Node2D, line_idx: int) -> Node2D:
+	# TODO: add texture instead of manual lines
+	line.edge_color = Color.WHITE_SMOKE
+	line.road_color = Color.BLACK
+	if line_idx % 2 == 0:
+		line.outer_color = Color.DARK_SLATE_GRAY
+	else:
+		line.outer_color = Color.SLATE_GRAY
+	return line
+
+func _generate_track_from_data() -> void:
+	print("Generating track from data")
+	var y_base = -480
+	for current_section in track_data:
+		for iline in range(int(current_section.length)):
+			print(str(iline))
+			var current_line = LineScene.instantiate()
+			current_line = _configure_line_colors(current_line, iline)
+			current_line.position.x = 0
+			current_line.position.y = current_line.line_width * iline + y_base
+			track_lines.append(current_line)
+			add_child(current_line)
+
+
+func _process(delta: float) -> void:
+	pass
