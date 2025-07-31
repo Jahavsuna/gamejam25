9daab57d9f7826b2e652b193f245b2fb418ea99b
Yahav Lavie <44113279+Jahavsuna@users.noreply.github.com>
2025-07-31 18:14:36 +0300

    Some curving now possible, seams between curves don't work

--- scripts/track_section_manager.gd
+++ scripts/track_section_manager.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
 const TRACK_DATA_FILE: String = "res://assets/tracks/first_level.json"
-var track_data: Array = []
 var track_lines: Array[Node2D] = []
+var track_data: Array = []
+var segment_ends: Array = []
+var active_segment = 0
 var screen_y_base: int = 0
 
+# TODO: rename once I manage to use those
 var segment_0: Dictionary = {
 	'y': 0,
 	'dy': 1,
@@ -28,9 +31,13 @@ func _load_track_data() -> void:
 	var file = FileAccess.open(TRACK_DATA_FILE, FileAccess.READ)
 	var json_string = file.get_as_text()
 	var parse_result = JSON.parse_string(json_string)
+	var distance_accumulator = 0
 	if parse_result is Array:
 		print("Loaded JSON, detected array")
-		track_data = parse_result
+		for p in parse_result:
+			track_data.append(p)
+			distance_accumulator += p["length"]
+			segment_ends.append(distance_accumulator)
 	else:
 		print("ERROR: Failed to load JSON from " + TRACK_DATA_FILE)
 	file.close()
@@ -48,20 +55,50 @@ func _generate_track() -> void:
 		add_child(current_line)
 		current_line.queue_redraw()
 
+func _extract_curve_params(segment: Dictionary) -> Array:
+	var dx: float
+	var ddx: float
+	if segment["type"] == "straight":
+		dx = 0
+		ddx = 0
+	if segment["type"] == "curve":
+		dx = segment["dx"]
+		ddx = segment["ddx"]
+	return [dx, ddx]
+
 func _physics_process(_delta: float) -> void:
+	# Avoid running this befor the function is ready
+	if not GameGlobals.is_screen_size_ready: return
+	
 	# Get player data to know how to advance
 	var translation_speed = GameGlobals.get_player_track_speed()
 	var start_coordinate = GameGlobals.get_player_track_coordinate()
 	var end_coordinate = start_coordinate + GameGlobals.LINES_PER_SCREEN
 	
+	# Advance segment if needed
+	if start_coordinate > segment_ends[active_segment]:
+		active_segment += 1
+	
 	# Advance all lines based on the speed
 	for line in track_lines:
 		line.translate(Vector2(0, translation_speed))
+		
 		# if the line leaves the screen, add a new line on top
 		if line.position.y - GameGlobals.LINE_WIDTH > screen_y_base:
 			line.position.y -= GameGlobals.LINES_PER_SCREEN * GameGlobals.LINE_WIDTH
+		
+		# Find segment for this line
+		var line_track_coord = start_coordinate + line.index_from_bottom()
+		var current_segment_parameters
+		# TODO: this may break when looping via the zones
+		if line_track_coord >= segment_ends[active_segment]:
+			current_segment_parameters = track_data[active_segment + 1]
+		else:
+			current_segment_parameters = track_data[active_segment]
+		
+		# Curve the line
+		var params: Array
+		params = _extract_curve_params(current_segment_parameters)
+		# If the line is part of a curve, act accordingly
+		line.curve(params[0], params[1])
 		line.update_size()
-	#var lines_to_plot = track_lines.slice(start_coordinate, end_coordinate)
-	# Called every frame
-	# draw the allowed number of lines
-	# trigonometry for line size
