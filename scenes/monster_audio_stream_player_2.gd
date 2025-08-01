e1a1fa762a4ca7e195e3cf3d04f44bc0e7869463
Yahav Lavie <44113279+Jahavsuna@users.noreply.github.com>
2025-08-01 15:49:36 +0300

    Cleanup of warnings

--- scenes/monster_audio_stream_player_2.gd
+++ scenes/monster_audio_stream_player_2.gd
@@ -13,7 +13,7 @@ func _ready() -> void:
 func _volume_db_from_coordinate_delta(coordinate_delta: float)-> float:
 	return 10-coordinate_delta
 	
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if self.playing: return
 	else: sound_i += 1
 	if sound_i >= len(sounds_array):
