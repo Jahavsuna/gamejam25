4ea0eb37660f85700ecbc6750b5a28bf52aa1999
Yahav Lavie <44113279+Jahavsuna@users.noreply.github.com>
2025-08-01 10:18:32 +0300

    Added defeat screen and attached it to defeat scenario

--- scenes/game_manager.gd
+++ scenes/game_manager.gd
@@ -5,11 +5,11 @@ var state: GameState = GameState.INIT
 
 func _ready() -> void:
 	state = GameState.TITLE
-	_apply_title()
+	_apply_title() # I think game manager only spawns after we leave the title screen but whatever
 	print("GameManager ready, state=" + str(state))
 
 func _apply_defeat() -> void:
-	pass
+	get_tree().change_scene_to_file("res://scenes/DefeatScreen.tscn")
 
 func _apply_victory() -> void:
 	pass
@@ -31,3 +31,5 @@ func _process(delta: float) -> void:
 	if player_coordinate >= track_end:
 		state = GameState.VICTORY
 		print("Player reached end! state=" + str(state))
+	
+	print("Monster distance: "+str(player_coordinate - monster_coordinate))
